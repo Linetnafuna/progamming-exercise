5bab53ada59fecc4f708aef7f0aeeeb0c687992b
Linetnafuna <160021211+Linetnafuna@users.noreply.github.com>
2024-02-23 19:59:32 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,14 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+int main(void)
 {
-    int a, b, c;
-    printf("Enter first value :");
-    scanf("%d" , &a);
-    printf("Enter second value :");
-    scanf("%d", &b);
-    c = a + b;
-    printf("%d + %d = %d\n" , a, b, c);
-    return 0;
+    float fahr, start, end, celcius, increment;
+
+
+   printf("Enter the start value in Fahr:");
+   scanf("%f", &start);
+   printf("Enter the end value in Fahr:");
+    scanf("%f", &end);
+    printf("Enter the increment value:");
+    scanf("%f", &increment);
+
+
+    printf("table of conversion of fahrnheiht to Celcius\n");
+    printf("\nfahrenheit\tcelcius\n");
+     printf("------\n");
+
+
+     for (fahr = start; fahr <= end; fahr += increment)
+     {
+         celcius = (fahr - 32) * 5 / 9;
+         printf("%8.2f\t%8.28.2f\n", fahr, celcius);
+     }
+     return 0;
+
+
 }
